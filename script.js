80dccb99c223a5f519444c4a0bcdba00a80a59e6
JemKeHU <staslovel1338@gmail.com>
2025-03-26 21:48:35 +1400

    created a computer choice algoritm

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Welcome to Rock Paper Scissors!");
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else if (computerChoice === 2) {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    return;
+}
